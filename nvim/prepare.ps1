6db897fa61d5fbf6c7cb494a2d8ecb3db8ef1626
Quillee <peraltamdb@gmail.com>
2022-07-04 10:23:12 -0400

    init

--- nvim/prepare.ps1
+++ nvim/prepare.ps1
@@ -1,6 +1,6 @@
-$home = $env:LOCALAPPDATA
+$home_appdata = $env:LOCALAPPDATA
 $nvim_plugin_folder = 'C:\tools\neovim\nvim-win64\share\nvim\runtime\plugin'
-$nvim_config_folder = '${home}\nvim'
+$nvim_config_folder = '${home_appdata}\nvim'
 $nvim_lua_folder = 'C:\tools\neovim\nvim-win64\bin\lua\'
 $folders =  @($nvim_plugin_folder, $nvim_config_folder, $nvim_lua_folder)
 
@@ -12,6 +12,10 @@ foreach ($n in $folders) {
     }
 }
 
-cp -r -Force plugins/*.vim $nvim_plugin_folder
-cp -r -Force plugins/*.lua  $nvim_lua_folder
+""
+"Copying files"
+cp -r -Force lua/*.vim $nvim_plugin_folder
+cp -r -Force lua/*.lua  $nvim_lua_folder
 cp -r -Force init.lua $nvim_config_folder
+"Done"
+
