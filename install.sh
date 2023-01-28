ba248c8495b2a5859b5cb5f95becdd1696b9057f
quillee <peraltamdb@gmail.com>
2023-01-27 21:19:23 -0500

    Finalized keymaps and settings

--- install.sh
+++ install.sh
@@ -4,32 +4,66 @@ curl -L https://nixos.org/nix/install | sh
 # source
 . ~/.nix-profile/etc/profile.d/nix.sh
 
+# install some things system-wide cause nix can't properly setup
+# Arch
+#
+# sudo pacman -Sy kitty clang firefox rofi
+#
+
+# echo locale archive env var for nix to know where to look
+echo "LOCALE_ARCHIVE=/usr/lib/locale-archive" >> /etc/profile
+
+
 # install packages
 nix-env -iA nixpkgs.nushell  \
-	nixpkgs.neovim   \
-	nixpkgs.kitty    \
-        nixpkgs.tmux     \
-	nixpkgs.zsh      \
-	nixpkgs.awesome  \
-	nixpkgs.antibody \
-	nixpkgs.fzf      \
-	nixpkgs.yarn     \
-	nixpkgs.ripgrep  \
-	nixpkgs.bat      \
-	nixpkgs.direnv   \
-	nixpkgs.lua      \
-	nixpkgs.clang    \
-        nixpkgs.rust     \
-	nixpkgs.zig
+    nixpkgs.libstdcxx5                    \
+    nixpkgs.nodejs                        \
+	nixpkgs.neovim                        \
+    nixpkgs.neovide                       \
+    nixpkgs.helix                         \
+    nixpkgs.tmux                          \
+	nixpkgs.zsh                           \
+	nixpkgs.awesome                       \
+	nixpkgs.antibody                      \
+	nixpkgs.fzf                           \
+	nixpkgs.yarn                          \
+	nixpkgs.ripgrep                       \
+	nixpkgs.bat                           \
+	nixpkgs.direnv                        \
+	nixpkgs.lua                           \
+    nixpkgs.sumneko-lua-language-server   \
+    nixpkgs.rnix-lsp                      \
+    nixpkgs.rust                          \
+    nixpkgs.cargo                         \
+	nixpkgs.zig                           \
+    nixpkgs.zls                           \
+    nixpkgs.autojump                      \
+    nixpkgs.spotify                       \
+    nixpkgs.spicetify-cli                 \
+    nixpkgs.nerdfonts                     \
+    nixpkgs.arandr                        \
+    nixpkgs.unzip                         \
+    nixpkgs.thefuck
 
 # install vim-plug
 curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-# change shell to zsh
-command -v zsh | sudo tee -a /etc/shells
-
-# default to zsh
-sudo chsh -s $(which zsh) $USER
+# copy neovim
+cp ./nvim ~/.config
 
 # zsh plugins
-antibody bundle < ./zsh/.zsh_plugins.txt > ~/.config/zsh/.zsh_plugins.sh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# copy themes
+./zsh/jovial/installer.sh
+cp ./zsh/theme-standalone/* $ZSH_CUSTOM/themes
+cp ./zsh/.zshrc ~
+
+# copy kitty config
+cp ./kitty ~/.config
+
+# copy helix
+cp ./helix ~/.config
+
+
+# copy tmux
+cp ./tmux ~/.config
+
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,3 +1,10 @@
+# load nix modules
+if [ -e ~/.nix-profile/etc/profile.d/nix.sh ]; then . ~/.nix-profile/etc/profile.d/nix.sh; fi # added by Nix installer
+
+# set options
+unsetopt BEEP
+
+
 # If you come from bash you might have to change your $PATH.
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 
@@ -8,7 +15,7 @@ export ZSH="$HOME/.oh-my-zsh"
 # load a random theme each time oh-my-zsh is loaded, in which case,
 # to know which specific one was loaded, run: echo $RANDOM_THEME
 # See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
-ZSH_THEME="lambda"
+ZSH_THEME="jovial"
 
 # Set list of themes to pick from when loading at random
 # Setting this variable when ZSH_THEME=random will cause zsh to load
@@ -72,22 +79,16 @@ ZSH_THEME="lambda"
 # Add wisely, as too many plugins slow down shell startup.
 plugins=(
   git
-  git-extras
-  vi-mode
-  themes
   autojump
   urltools
   bgnotify
   zsh-autosuggestions
   zsh-syntax-highlighting
   jovial
-  1password
-  colored-man-pages
-  command-not-found
-  colorize
-  rust
-  ripgrep
+  vi-mode
   z
+  tmux
+  thefuck
 )
 
 source $ZSH/oh-my-zsh.sh
@@ -117,3 +118,6 @@ source $ZSH/oh-my-zsh.sh
 # Example aliases
 # alias zshconfig="mate ~/.zshrc"
 # alias ohmyzsh="mate ~/.oh-my-zsh"
+#
+
+#
